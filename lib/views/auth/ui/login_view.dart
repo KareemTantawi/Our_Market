802cbd356c66c0b3dd45bf90a0c6619c1003159b
Kareem Tantawi <kareem01229526319@gmail.com>
2024-12-17 17:56:32 +0200

    create and refactor text form field

--- lib/views/auth/ui/login_view.dart
+++ lib/views/auth/ui/login_view.dart
@@ -1,10 +1,115 @@
 import 'package:flutter/material.dart';
+import 'package:our_market/core/app_colors.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Welcome To Our Market',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Card(
+              color: AppColors.kWhiteColor,
+              margin: EdgeInsets.all(24),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(12),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  spacing: 24,
+                  children: [
+                    CustomTextFormField(
+                      labelText: 'Email',
+                    ),
+                    // SizedBox(
+                    //   height: 24,
+                    // ),
+                    CustomTextFormField(
+                      labelText: 'Password',
+                      isSecure: true,
+                      suffixIcon: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.visibility_off,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomTextFormField extends StatelessWidget {
+  const CustomTextFormField({
+    super.key,
+    required this.labelText,
+    this.suffixIcon,
+    this.isSecure = false,
+  });
+  final String labelText;
+  final Widget? suffixIcon;
+  final bool isSecure;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      obscureText: isSecure,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'This Field is Required';
+        }
+        return null;
+      },
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        labelText: labelText,
+        suffixIcon: suffixIcon,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(
+            width: 2,
+            color: AppColors.kBordersideColor,
+          ),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(
+            width: 2,
+            color: AppColors.kBordersideColor,
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(
+            width: 2,
+            color: AppColors.kBordersideColor,
+          ),
+        ),
+      ),
+    );
   }
 }
