743891a31d82187aa156ce6c52b1f7bc29b22c92
Kareem Tantawi <kareem01229526319@gmail.com>
2024-12-21 17:59:35 +0200

    create the product card

--- lib/views/home/ui/home_view.dart
+++ lib/views/home/ui/home_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:our_market/core/app_colors.dart';
 import 'package:our_market/core/components/custom_search_field.dart';
 import 'package:our_market/views/home/ui/widgets/custom_category_widget.dart';
 
@@ -43,6 +42,35 @@ class HomeView extends StatelessWidget {
               },
             ),
           ),
+          SizedBox(
+            height: 15,
+          ),
+          Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(16)),
+            ),
+            child: Column(
+              children: [
+                Stack(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.only(
+                        // topLeft: Radius.circular(16),
+                        topRight: Radius.circular(16),
+                        bottomLeft: Radius.circular(16),
+                        bottomRight: Radius.circular(16),
+                      ),
+                      child: Image(
+                        image: NetworkImage(
+                          "https://img.freepik.com/free-vector/simple-product-3d-background-vector-set_53876-156228.jpg?t=st=1734796521~exp=1734800121~hmac=5abe1958b732508560e129f31091dbfbbc475c0557e9ddf92a8e74bc5a99a2de&w=996",
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
